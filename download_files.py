ad36d9e6b3e03cf462548d07f6667f900433514f
Blane <161300198+Blane187@users.noreply.github.com>
2024-03-15 02:20:47 +0700

    Create download_files.py

---
+++ download_files.py
@@ -0,0 +1,16 @@
+import subprocess, os
+assets_folder = "./assets/"
+if not os.path.exists(assets_folder):
+    os.makedirs(assets_folder)
+files = {
+    "rmvpe/rmvpe.pt":"https://huggingface.co/Rejekts/project/resolve/main/rmvpe.pt",
+    "hubert/hubert_base.pt":"https://huggingface.co/Rejekts/project/resolve/main/hubert_base.pt",
+    "fcpe/fcpe.py":"https://huggingface.co/IAHispano/Applio/blob/main/Resources/fcpe.pt"
+}
+for file, link in files.items():
+    file_path = os.path.join(assets_folder, file)
+    if not os.path.exists(file_path):
+        try:
+            subprocess.run(['wget', link, '-O', file_path], check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Error downloading {file}: {e}")
